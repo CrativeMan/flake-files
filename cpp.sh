2a9ac8625701e1b21bc31fefd5c36cae3225bf31
crative <hannig.sebi@gmail.com>
2024-05-11 18:50:01 +0200

    cpp

--- cpp.sh
+++ cpp.sh
@@ -1,6 +1,6 @@
 # copying flake.nix and .envrc files to current dir
-cp ~/.flake-files/c/flake.nix ./ 
-cp ~/.flake-files/c/.envrc ./
+cp ~/.flake-files/cpp/flake.nix ./ 
+cp ~/.flake-files/cpp/.envrc ./
 
 # allowing direnv
 direnv allow
